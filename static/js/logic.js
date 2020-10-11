69e8fbd234565e359d45851e6d9b599083c4ab50
ckunakom <ckunakom@gmail.com>
2020-10-11 13:16:33 -0500

    map failed but no error

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,76 +2,116 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(earthquakeData) {
+d3.json(queryUrl, function(geodata) {
 
-})
+    // console.log(earthquakeData);
+    // Send features object from earthquateData to createFeatures fuction
+    createFeatures(geodata.features);
+});
 
-// Code from class exercise
-// createFeatures(data.features);
-// });
+// Function to create geoJSON layer and popup box
+function createFeatures(earthquakeData) {
+    // console.log(earthquakeData);
 
-// function createFeatures(earthquakeData) {
+  // Define a function to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
 
-//   // Define a function we want to run once for each feature in the features array
-//   // Give each feature a popup describing the place and time of the earthquake
-//   function onEachFeature(feature, layer) {
-//     layer.bindPopup("<h3>" + feature.properties.place +
-//       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-//   }
+    //   console.log(feature, layer);
+  }
+//     feature.properties.mag --> give's the magnitude or just use .title for place & mag
 
-//   // Create a GeoJSON layer containing the features array on the earthquakeData object
-//   // Run the onEachFeature function once for each piece of data in the array
-//   var earthquakes = L.geoJSON(earthquakeData, {
-//     onEachFeature: onEachFeature
-//   });
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
 
-//   // Sending our earthquakes layer to the createMap function
-//   createMap(earthquakes);
-// }
+//   console.log(earthquakes);
 
-// function createMap(earthquakes) {
+  // Sending earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
 
-//   // Define streetmap and darkmap layers
-//   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id: "mapbox/streets-v11",
-//     accessToken: API_KEY
-//   });
+// Function to create map 
+function createMap(earthquakes) {
 
-//   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "dark-v10",
-//     accessToken: API_KEY
-//   });
+  // Define grayscale, satellite and outdoors layers
+  var grayscalemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+  
+  var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "satellite-v9",
+    accessToken: API_KEY
+  });
 
-//   // Define a baseMaps object to hold our base layers
-//   var baseMaps = {
-//     "Street Map": streetmap,
-//     "Dark Map": darkmap
-//   };
+  var outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "outdoors-v11",
+    accessToken: API_KEY
+  });
 
-//   // Create overlay object to hold our overlay layer
-//   var overlayMaps = {
-//     Earthquakes: earthquakes
-//   };
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    Grayscale: grayscalemap,
+    Satellite: satellitemap,
+    Outdoor: outdoormap,
+  };
 
-//   // Create our map, giving it the streetmap and earthquakes layers to display on load
-//   var myMap = L.map("map", {
-//     center: [
-//       37.09, -95.71
-//     ],
-//     zoom: 5,
-//     layers: [streetmap, earthquakes]
-//   });
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    // "Techtonic Plates": techtonicplates,
+    "Earthquakes": earthquakes,
+  };
 
-//   // Create a layer control
-//   // Pass in our baseMaps and overlayMaps
-//   // Add the layer control to the map
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap);
-// }
+  // Create our map, giving it the satellite map and techtonic plates + earthquakes layers to display on load
+  var myMap = L.map("mapid", {
+    center: [15.5994, -28.6731],
+    zoom: 5,
+    layers: [satellitemap, earthquakes]
+  });
+  console.log(myMap);
+
+  // Create a layer control with baseMaps and overlayMaps
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
+
+// // use heat layer to get the circles! class ex)
+// // NEED LEAFLET PLUG IN
+// // <!-- Leaflet heatmap plugin-->
+// // <script type="text/javascript" src="static/js/leaflet-heat.js"></script>
+// // d3.json(url).then(function(response) {
+
+// //     console.log(response);
+  
+// //     var heatArray = [];
+  
+// //     for (var i = 0; i < response.length; i++) {
+// //       var location = response[i].location;
+  
+// //       if (location) {
+// //         heatArray.push([location.coordinates[1], location.coordinates[0]]);
+// //       }
+// //     }
+  
+// //     var heat = L.heatLayer(heatArray, {
+// //       radius: 20,
+// //       blur: 35 -- use 1! or take radius 10 blur 3
+// //     }).addTo(myMap);
+  
+// //   });
+
+////////-------------------------------------------------
