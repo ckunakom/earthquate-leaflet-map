182e1f4813670e68cf736e5d93c0aa9d719c97b2
ckunakom <ckunakom@gmail.com>
2021-01-02 13:41:04 -0600

    Added public token for map

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,21 +6,21 @@ var grayscalemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/light-v10",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiY2t1bmFrb20iLCJhIjoiY2tmeWFtd3NtMDJtcTMxcGlwcmYzanZkYSJ9.Q8sNDdN5T7hZKmwfbzHjqA"
 });
 
 var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "satellite-v9",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiY2t1bmFrb20iLCJhIjoiY2tmeWFtd3NtMDJtcTMxcGlwcmYzanZkYSJ9.Q8sNDdN5T7hZKmwfbzHjqA"
 });
 
 var outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "outdoors-v11",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiY2t1bmFrb20iLCJhIjoiY2tmeWFtd3NtMDJtcTMxcGlwcmYzanZkYSJ9.Q8sNDdN5T7hZKmwfbzHjqA"
 });
 
 // Define the two layer groups
